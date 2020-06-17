08c144f1f946def31e41345a7fbfe9e44ae895c7
bgreen53 <green.barry53@gmail.com>
2020-06-16 21:47:18 -0700

    created auth js and test html

---
+++ auth.js
@@ -0,0 +1,139 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAqo3WdF_RkKrfhRi_5GcfdIpehsTRcxrk",
+    authDomain: "project3-9a5b0.firebaseapp.com",
+    databaseURL: "https://project3-9a5b0.firebaseio.com",
+    projectId: "project3-9a5b0",
+    storageBucket: "project3-9a5b0.appspot.com",
+    messagingSenderId: "828265317184",
+    appId: "1:828265317184:web:623c24ba65f1528d6db0a5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+//make and store firestore references 
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+
+//create new account click funciton to grab email and pw
+$(document).ready(function(){
+$("#add-btn").on("click", function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    const email = $("#email").val().trim()
+    const password = $("#signup-password").val().trim()
+
+    //pass info to firebase
+        auth.createUserWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode === "auth/email-already-in-use") {
+            alert("email already in use")
+            
+            
+        }
+
+    }).then(function (cred) {
+
+        console.log(cred)
+
+        if(!cred){
+            return;
+        }
+  
+        
+        
+        
+       
+        //  $.ajax({
+        //      url: "/api/org",
+        //      method: "POST",
+        //       data : 
+        //  }).then(function(data){
+        //      console.log(data)
+         //})
+        alert("Account Creation Successful!")
+             
+        
+    })
+
+
+});
+
+$("#signOut").on("click", function (event) {
+    event.preventDefault()
+
+    auth.signOut().then(function () {
+        console.log("user signed out")
+    })
+
+});
+
+
+    $("#login").on("click", function (event) {
+        event.preventDefault()
+        event.stopPropagation()
+        console.log("clicked")
+
+        const email = $("#loginEmail").val()
+        const password = $("#loginPassword").val()
+        auth.signInWithEmailAndPassword(email, password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(errorCode + " " + errorMessage)
+            if(errorCode==="auth/user-not-found"){
+                alert("There is no account linked to that email. Please go to the Create a Login page to sign up.") 
+
+            }
+            else if(errorCode==="auth/wrong-password"){
+                alert("Your password is incorrect. Click forgot password for assistance.")
+                
+            }
+
+
+        }).then(function (cred) {
+
+            if(!cred){
+                return;
+            }
+
+            console.log(cred)
+            alert("Sign in Successful!")
+            
+
+        })
+
+    })
+
+
+$("#forgotPW").on("click", function (event) {
+    event.preventDefault()
+
+var emailAddress = $("#loginEmail").val().trim();
+
+auth.sendPasswordResetEmail(emailAddress).then(function() {
+  // Email sent.
+  console.log("email sent")
+}).catch(function(error) {
+  // An error happened.
+});
+
+});
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          console.log("user is signed in")
+          
+        } else {
+          // User is signed out.
+          // ...
+
+          console.log("user is signed out")
+        }
+      });
+
+    })
