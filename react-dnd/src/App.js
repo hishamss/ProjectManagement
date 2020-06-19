ebc52b0009c8e0056f0fdadda72ad5c5c5b9e9f9
Dylan Ambrose <djambro44@gmail.com>
2020-06-19 16:51:59 -0700

    added more boards

--- react-dnd/src/App.js
+++ react-dnd/src/App.js
@@ -7,19 +7,33 @@ function App() {
   return (
     <div className="App">
       <main className="flexbox">
-        <Board id= "board-1" className="Board">
-          <Card id= "card-1" className="Card" draggable = "true">
+        <Board id="board-1" className="Board">
+          <Card id="card-1" className="Card" draggable="true">
             <p>card 1</p>
           </Card>
         </Board>
-Hello
-        <Board id= "board-2" className="Board">
-          <Card id= "card-2" className="Card" draggable = "true">
+
+        <Board id="board-2" className="Board">
+          <Card id="card-2" className="Card" draggable="true">
             <p>card 2</p>
           </Card>
         </Board>
-      </main>
 
+        <Board id="board-3" className="Board">
+          <Card id="card-3" className="Card" draggable="true">
+            <p>card 2</p>
+          </Card>
+        </Board>
+
+        <Board id="board-4" className="Board">
+          <Card id="card-4" className="Card" draggable="true">
+            <p>card 2</p>
+          </Card>
+        </Board>
+
+        
+
+      </main>
     </div>
   );
 }
