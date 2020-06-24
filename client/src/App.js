b2afd76a84bd123603f002de377392948b5c2aa4
Barry Green <Barry.Green@SLFHC.ORG>
2020-06-24 16:36:38 -0700

    added router, basic layout for landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Checkout from "./components/Checkout";
+import Landing from "./components/Landing";
 
 function App() {
-  return <Checkout />;
+  return(
+  <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/checkout" component={Checkout} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
