a3b266d81a5549e9d58586d80646764abf6f0b9f
hishamss <hishamss90@gmail.com>
2020-06-29 16:36:16 -0700

    styled the modal and added email validation

--- client/src/pages/Projects/index.js
+++ client/src/pages/Projects/index.js
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
 import API from "../../utils/API";
 import { Modal } from "react-bootstrap";
+import "./style.css";
 function Projects() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => {
     setMessage("");
     setShow(true);
   };
   const handleSubmit = (event) => {
+    setMessage("");
     event.preventDefault();
-    API.sendEmail(email)
-      .then(() => {
-        setMessage("Sent");
-      })
-      .catch(() => {
-        setMessage("Error, try again!");
-      });
+    if (email) {
+      setLoading(true);
+
+      API.sendEmail(email)
+        .then(() => {
+          setLoading(false);
+          setMessage("Sent");
+          setEmail("");
+        })
+        .catch(() => {
+          setLoading(false);
+          setMessage("Error, try again!");
+          setEmail("");
+        });
+    } else {
+      setMessage("Please provide email !!");
+    }
   };
 
   const handleInputChange = (event) => {
@@ -38,24 +51,33 @@ function Projects() {
 
         <Modal.Body>
           <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label>
-                Enter the email address of the user you would like to add
-              </label>
+            <div className="form-group" style={{ marginBottom: 0 }}>
+              <label>Add your team</label>
               <input
+                value={email}
                 name="email"
                 type="email"
                 onChange={handleInputChange}
                 className="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
+                placeholder="Email address for user"
               />
             </div>
-
-            <button type="submit" className="btn btn-primary">
-              Submit
+            <img
+              style={{ width: "70px" }}
+              src="https://assets.materialup.com/uploads/163595e3-140e-4334-af76-cf7902795c51/preview.gif"
+              alt="loading"
+              className={loading ? "showLoading" : "hideLoading"}
+            ></img>
+            <p style={{ marginTop: "1rem" }}>{message}</p>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              style={{ background: "#4bb3fd", display: "block" }}
+            >
+              Invite
             </button>
-            <p>{message}</p>
           </form>
         </Modal.Body>
       </Modal>
