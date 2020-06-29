8a5e3a2eb768cf7bb3c4a8446beb67b8aad1e54f
hishamss <hishamss90@gmail.com>
2020-06-28 17:22:57 -0700

    created adduser route

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -4,14 +4,18 @@ const projectsController = require("../../controllers/projectController");
 router
   .route("/")
   .get(projectsController.findAll)
-  .post(projectsController.create)
+  .post(projectsController.create);
 
+router.route("/adduser/:user").get((req, res) => {
+  console.log(`from Email: ${req.params.user}`);
+  res.send("you have been Added");
+});
 router
   .route("/:id")
   .get(projectsController.findOne)
   .delete(projectsController.delete)
   .put(projectsController.update);
 
-  router.post('/add-user-to-project', projectsController.addUserToProject)
+router.post("/add-user-to-project", projectsController.addUserToProject);
 
 module.exports = router;
