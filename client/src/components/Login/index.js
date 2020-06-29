a887397392a2a241f695bc13c62b3471c676d6d6
bgreen53 <green.barry53@gmail.com>
2020-06-28 21:03:03 -0700

    added state and onClick for forgot pw

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,47 +1,85 @@
-import React, { useCallback, useContext } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import { withRouter, Redirect } from "react-router";
 import app from "../../Base";
 import { AuthContext } from "../../Auth.js";
 
+
 const Login = ({ history }) => {
-    const handleLogin = useCallback(
+
+    const [Email, setEmail] = useState('');
+    const isInvalid = Email ===''
+
+    const handleSubmit = useCallback(
         async event => {
             event.preventDefault();
             const { email, password } = event.target.elements;
             try {
+                
                 await app
                     .auth()
-                    .signInWithEmailAndPassword(email.value, password.value);
-                history.push("/");
+                    .signInWithEmailAndPassword(email.value, password.value)
+                    .then(()=>{
+                        if({history}){
+                        history.push("/");
+                        }else{
+                           return <Redirect to="/home" />
+                        }
+                    })
+                
             } catch (error) {
-                alert(error);
+                switch (error){
+                    case "auth/invalid-email":
+                       alert("Please enter a valid email address")
+                        break;
             }
+            } 
         },
         [history]
     );
 
+    const forgotPw = useCallback(
+       
+           
+        async event => {
+            event.preventDefault();
+         
+            try {
+                await app
+                    .auth()
+                    .sendPasswordResetEmail(Email)
+                    .then(alert("A password reset email has been sent to " + Email))
+                
+            } catch (error) {
+                alert(error);
+            }
+        }
+    
+
+    );
+
+
     const { currentUser } = useContext(AuthContext);
 
     if (currentUser) {
-        return <Redirect to="/" />;
+        return <Redirect to="/home" />;
     }
 
     return (
         <div>
-            <form onSubmit={handleLogin}>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" name="email" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" onChange={e => setEmail(e.target.value)} />
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Password</label>
                     <input type="password" className="form-control" name="password" />
                 </div>
                 <button type="submit" className="btn btns w-25 mx-5 px-4" id="login" >Login</button>
-                <button type="submit" className="btn btns w-50">Forgot Password</button>
+                <button type="push" className="btn btns w-50" id="forgot" disabled={isInvalid} onClick={forgotPw} >Forgot Password</button>
             </form>
         </div>
     );
-};
+}
 
 export default withRouter(Login);
--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -31,21 +31,7 @@ const Landing = ({history}) => {
         }
       }, [history]);
 
-    const handleLogin = useCallback(
-        async event => {
-          event.preventDefault();
-          const { email, password } = event.target.elements;
-          try {
-            await app
-              .auth()
-              .signInWithEmailAndPassword(email.value, password.value);
-            history.push("/");
-          } catch (error) {
-            alert(error);
-          }
-        },
-        [history]
-      );
+    
 
     return (
         
