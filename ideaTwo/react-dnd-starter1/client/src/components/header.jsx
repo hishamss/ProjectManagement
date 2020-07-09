0124ecfe4c9fef5ada2696d951c604df4aa4c00b
Dylan Ambrose <djambro44@gmail.com>
2020-07-08 21:03:14 -0700

    styled kanban board with project colors

--- ideaTwo/react-dnd-starter1/client/src/components/header.jsx
+++ ideaTwo/react-dnd-starter1/client/src/components/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <div className={"row"}>
-            <p className={"page-header"}>Done.</p>
+            <p className={"page-header"}><img src="/img/Done.png" alt="done logo"></img></p>
         </div>
     )
 
