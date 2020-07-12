6ca44ee1a74ae50271213d1d5cf32054afcf61fc
hishamss <hishamss90@gmail.com>
2020-07-11 21:06:09 -0700

    wrap everthing inside react fragment

--- client/src/pages/kanban/Homepage.js
+++ client/src/pages/kanban/Homepage.js
@@ -3,7 +3,7 @@ import Item from "./item";
 import DropWrapper from "./DropWrapper";
 import Col from "./col";
 import { data, statuses } from "./data";
-import styles from './main.css';
+import styles from "./main.css";
 
 const Homepage = () => {
   const [items, setItems] = useState(data);
@@ -29,36 +29,39 @@ const Homepage = () => {
   };
 
   return (
-    <div className={"row"}>
-      {statuses.map((s) => {
-        return (
-          <div key={s.status} className={"col-wrapper"}>
-            <h2 className={"col-header"}>{s.status.toUpperCase()}</h2>
-            <DropWrapper
-              onDrop={onDrop}
-              status={s.status}
-              setItems={setItems}
-              items={items}
-            >
-              <Col>
-                {items
-                  .filter((i) => i.status === s.status)
-                  .map((i, idx) => (
-                    <Item
-                      key={i.id}
-                      item={i}
-                      index={idx}
-                      moveItem={moveItem}
-                      status={s}
-                    />
-                  ))}
-              </Col>
-            </DropWrapper>
-          </div>
-        );
-      })}
-      )
-    </div>
+    <React.Fragment>
+      <h1 style={{ color: "black" }}>test</h1>
+      <div className={"row"}>
+        {statuses.map((s) => {
+          return (
+            <div key={s.status} className={"col-wrapper"}>
+              <h2 className={"col-header"}>{s.status.toUpperCase()}</h2>
+              <DropWrapper
+                onDrop={onDrop}
+                status={s.status}
+                setItems={setItems}
+                items={items}
+              >
+                <Col>
+                  {items
+                    .filter((i) => i.status === s.status)
+                    .map((i, idx) => (
+                      <Item
+                        key={i.id}
+                        item={i}
+                        index={idx}
+                        moveItem={moveItem}
+                        status={s}
+                      />
+                    ))}
+                </Col>
+              </DropWrapper>
+            </div>
+          );
+        })}
+        )
+      </div>
+    </React.Fragment>
   );
 };
 
